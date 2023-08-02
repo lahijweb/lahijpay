5498441372da1b7c4ab853e9d1cec69e7ab01195
Amir Fallah <infoamirfallah@gmail.com>
2023-08-02 17:01:17 +0330

    Added Gateway migration & model

---
+++ database/migrations/2023_08_02_132451_create_gateways_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('gateways', function (Blueprint $table) {
+            $table->id();
+            $table->string('driver');
+            $table->string('name');
+            $table->text('config');
+            $table->boolean('is_default');
+            $table->boolean('is_active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('gateways');
+    }
+};
