8dd54e365b27e4fb5beed7b323df9cdf090ab399
Amir Fallah <infoamirfallah@gmail.com>
2023-08-02 17:07:37 +0330

    Added payment migration & model

---
+++ database/migrations/2023_08_02_133221_create_payments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->uuid()->unique();
+            $table->foreignId('gateway_id')->constrained()->cascadeOnUpdate();
+            $table->decimal('amount', 24, 4)->default(0.0000);
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('email')->nullable();
+            $table->string('mobile')->nullable();
+            $table->string('description')->nullable();
+            $table->enum('status', ['PENDING', 'ACCEPTED', 'REJECTED'])->default('PENDING');
+            $table->string('transactionid')->nullable();
+            $table->string('referenceid')->nullable();
+            $table->timestamp('verified_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
