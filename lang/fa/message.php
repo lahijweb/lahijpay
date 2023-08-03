be5d7e91530429cd46f58d984ed578732798658f
Amir Fallah <infoamirfallah@gmail.com>
2023-08-03 19:23:13 +0330

    Added fa message lang

---
+++ lang/fa/message.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'gateway_invalid' => 'درگاه نامعتبر است.',
+    'transaction_not_found' => 'تراکنش یافت نشد.',
+    'transaction_not_pending' => 'تراکنش قبلا بررسی شده است.',
+    'transaction_verified' => 'تراکنش با موفقیت انجام شد.',
+    'transaction_not_verified' => 'تراکنش با خطا مواجه شد.',
+
+];
