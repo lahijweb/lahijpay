b4b4cb5e0a5040ec5408f31b20d63c6fb87329f8
Amir Fallah <infoamirfallah@gmail.com>
2023-10-01 11:47:15 +0330

    Changed payment status enum

--- app/Enums/PaymentStatusEnum.php
+++ app/Enums/PaymentStatusEnum.php
@@ -2,9 +2,31 @@
 
 namespace App\Enums;
 
-enum PaymentStatusEnum:string
+use Filament\Support\Contracts\HasColor;
+use Filament\Support\Contracts\HasLabel;
+
+enum PaymentStatusEnum: string implements HasLabel, HasColor
 {
-    case PENDING = 'PENDING';
-    case  ACCEPTED = 'ACCEPTED';
-    case  REJECTED = 'REJECTED';
+    case Pending = 'PENDING';
+    case Accepted = 'ACCEPTED';
+    case Rejected = 'REJECTED';
+
+
+    public function getLabel(): ?string
+    {
+        return match ($this) {
+            self::Pending => 'در حال بررسی',
+            self::Accepted => 'تایید شده',
+            self::Rejected => 'رد شده',
+        };
+    }
+
+    public function getColor(): string|array|null
+    {
+        return match ($this) {
+            self::Pending => 'gray',
+            self::Accepted => 'success',
+            self::Rejected => 'warning',
+        };
+    }
 }
