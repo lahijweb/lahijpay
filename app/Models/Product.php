cbd794675ca9cf8dc532b84251114c97deae1dc2
Amir Fallah <infoamirfallah@gmail.com>
2023-10-14 16:06:15 +0330

    Added softdeletes to model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,6 +6,7 @@ use App\Enums\ProductStatusEnum;
 use App\Enums\ProductTypeEnum;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
