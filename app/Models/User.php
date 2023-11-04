00436be8f7202a9f7d773f0a7f482cfc97c08aa6
Amir Fallah <infoamirfallah@gmail.com>
2023-11-04 18:07:39 +0330

    Changed canAccessPanel

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,7 +48,7 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@gmail.com') && $this->hasVerifiedEmail();
+        return true;
     }
 
     public function getFilamentName(): string
